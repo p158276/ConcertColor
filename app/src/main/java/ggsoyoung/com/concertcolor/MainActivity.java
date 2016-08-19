f8fde11d9bf2be32fe9702fc59529a225d484e6c
ayou7995 <ayou7995@gmail.com>
2016-08-19 12:53:06 +0800

    Add broadcast reciver and intent filter.

--- app/src/main/java/ggsoyoung/com/concertcolor/MainActivity.java
+++ app/src/main/java/ggsoyoung/com/concertcolor/MainActivity.java
@@ -1,15 +1,26 @@
 package ggsoyoung.com.concertcolor;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+	final String Tag = "Jonathan";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+        IntentFilter mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(MyFirebaseMessagingService.MESSAGE_RECEIVE);
+        registerReceiver(mReceiver, mIntentFilter);
 	}
 
 //	@Override
@@ -21,8 +32,18 @@ public class MainActivity extends AppCompatActivity {
 //			Log.d("FCM", "msg:"+msg);
 //	}
 
-	public void onMsgGet(){
-		Log.d("TAG","GETIT");
-	}
+	// public void onMsgGet(){Log.d("TAG","GETIT");}
+
+	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			final String action = intent.getAction();
+			Log.i(Tag, "broadcastReceiver: " + action);
+            if(MyFirebaseMessagingService.MESSAGE_RECEIVE.equals(action)){
+                LinearLayout bgElement = (LinearLayout) findViewById(R.id.container);
+                bgElement.setBackgroundColor(getResources().getColor(R.color.background_Blue));
+            }
+		}
+	};
 
 }
--- app/src/main/java/ggsoyoung/com/concertcolor/MyFirebaseMessagingService.java
+++ app/src/main/java/ggsoyoung/com/concertcolor/MyFirebaseMessagingService.java
@@ -1,5 +1,6 @@
 package ggsoyoung.com.concertcolor;
 
+import android.content.Intent;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -11,6 +12,8 @@ import com.google.firebase.messaging.RemoteMessage;
  */
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
+	final String Tag = "Jonathan";
+	public final static String MESSAGE_RECEIVE = "MsgReceive";
 	@Override
 	public void onMessageReceived(RemoteMessage remoteMessage) {
 		// ...
@@ -27,11 +30,17 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 		// Check if message contains a notification payload.
 		if (remoteMessage.getNotification() != null) {
 			Log.d(Constant.TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+			broadcastUpdate(MESSAGE_RECEIVE,remoteMessage.getNotification().getBody());
 		}
 
 		// Also if you intend on generating your own notifications as a result of a received FCM
 		// message, here is where that should be initiated. See sendNotification method below.
 	}
-
+	private void broadcastUpdate(final String action, final String msg) {
+		Log.i(Tag, "broadcastUpdate: " + action + " message : " + msg);
+		final Intent intent = new Intent(action);
+		intent.putExtra(MESSAGE_RECEIVE, msg);
+		sendBroadcast(intent);
+	}
 }
 
